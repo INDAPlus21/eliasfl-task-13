ee56751fad1b2c68b202ef242c7aea1472be7c00
Elias <elias@floreteng.se>
2022-01-21 06:26:20 +0100

    Normal colored sphere

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,63 @@
 use eliasfl_task_13::*;
 
-fn hit_sphere(center: Point3D, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: Point3D, radius: f64, ray: &Ray) -> f64 {
     let circ_origin = ray.origin - center;
-    let a = ray.direction * ray.direction;
-    let b = 2.0 * circ_origin * ray.direction;
-    let c = circ_origin * circ_origin - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    let a = ray.direction.length_squared();
+    let half_b = circ_origin * ray.direction;
+    let c = circ_origin.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-half_b - discriminant.sqrt()) / a
+    }
 }
 
 fn ray_color(ray: &Ray) -> [f64; 3] {
-    if hit_sphere(Point3D::new(0.0, 0.0, 2.0), 0.5, ray) {
-        return [1.0, 0.0, 0.0];
+    let center = Point3D::new(0.0, 0.0, -1.0);
+    let hit = hit_sphere(center, 0.5, ray);
+    if hit > 0.0 {
+        let normal = (ray.at(hit) - center).normalize();
+        return [
+            0.5 * (normal.x + 1.0),
+            0.5 * (normal.y + 1.0),
+            0.5 * (normal.z + 1.0),
+        ];
     }
     let t = 0.5 * (ray.direction.normalize().y + 1.0);
-    // From white to blue
     [
-        (t * 1.0 + t * 0.5),
-        (t * 1.0 + t * 0.7),
-        (t * 1.0 + t * 1.0),
+        ((1.0 - t) * 1.0 + t * 0.5),
+        ((1.0 - t) * 1.0 + t * 0.7),
+        ((1.0 - t) * 1.0 + t * 1.0),
     ]
 }
 
 #[test]
 fn test_ray_color() {
     let p = Point3D::new(0.0, 0.0, 0.0);
-    let q = Point3D::new(1.0, 0.0, 0.0);
+    let q = Point3D::new(1.0, 0.0, 1.0);
     let r = Ray::new(p, q);
     assert_eq!(ray_color(&r), [0.75, 0.85, 1.0]);
 }
 
 #[test]
 fn test_ray() {
-    let cam = Camera::standard(1.0, 1.0);
+    let cam = Camera::standard(2.0, 2.0);
     let ray = cam.get_ray(0.5, 1.0);
     println!("{:?}", ray_color(&ray));
 }
 
 fn main() {
-    let (width, height) = (256, 256);
+    let (width, height) = (1024, 512);
+    let scale = 4.0;
     let mut img: image::RgbImage = image::ImageBuffer::new(width, height);
-    let cam = Camera::standard(1.0, 1.0);
+    let cam = Camera::standard(scale, (height as f64 / width as f64) * scale);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         let u = x as f64 / width as f64;
         let v = y as f64 / height as f64;
         let ray = cam.get_ray(u, v);
+        // Convert to u8 from float and set pixel
         *pixel = image::Rgb(ray_color(&ray).map(|v| (v * u8::MAX as f64).round() as u8));
     }
 
--- src/scene.rs
+++ src/scene.rs
@@ -28,9 +28,9 @@ impl Camera {
 
     pub fn standard(width: f64, height: f64) -> Self {
         Camera::new(
-            Camera::origin(),
+            Point3D::new(0.0, 0.0, 0.0),
             Point3D::new(0.0, 0.0, 1.0),
-            Point3D::new(0.0, height, 0.0),
+            Point3D::new(0.0, -height, 0.0),
             Point3D::new(width, 0.0, 0.0),
         )
     }
@@ -38,18 +38,22 @@ impl Camera {
     pub fn get_ray(&self, u: f64, v: f64) -> Ray {
         Ray::new(
             self.position,
-            self.lower_left() - self.direction
-                + self.horizontal * u
-                + self.vertical * v
-                + Self::origin(),
+            self.lower_left() + self.horizontal * u + self.vertical * v - self.position,
         )
     }
 
-    pub fn origin() -> Point3D {
-        Point3D::new(0.0, 0.0, 0.0)
-    }
-
     pub fn lower_left(&self) -> Point3D {
-        self.position - 0.5 * self.vertical - 0.5 * self.horizontal
+        self.position - self.vertical / 2.0 - self.horizontal / 2.0 - self.direction
     }
+}
+
+#[test]
+fn test_camera() {
+    let cam = Camera::standard(2.0, 2.0);
+    let result = Point3D {
+        x: -1.0,
+        y: 1.0,
+        z: -1.0,
+    };
+    assert_eq!(cam.lower_left(), result);
 }
